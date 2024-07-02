7cc7f3985788f441668f7dcbca19d43ac9fedc3f
datta <datta.r.pwr@gmail.com>
2024-07-02 15:45:16 +0530

    implemented authentication and circuit breaker.

--- inventory-service/src/main/java/com/glo/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/glo/inventory_service/service/InventoryService.java
@@ -9,16 +9,23 @@ import com.glo.inventory_service.dto.InventoryResponse;
 import com.glo.inventory_service.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 	
 	private final InventoryRepository inventoryRepository;
 	
 	@Transactional(readOnly = true)
+	@SneakyThrows
 	public List<InventoryResponse> isInStock(List<String> skuCode)
 	{
+		log.info("Wait Started");
+		Thread.sleep(10000);
+		log.info("Wait Ended");
 		return inventoryRepository.findBySkuCodeIn(skuCode)
 				.stream()
 				.map(inventory -> 
--- order-service/src/main/java/com/glo/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/glo/orderservice/controller/OrderController.java
@@ -1,30 +1,44 @@
 package com.glo.orderservice.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 import com.glo.orderservice.dto.OrderRequest;
 import com.glo.orderservice.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 	
 	private final OrderService orderService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public String placeOrder(@RequestBody OrderRequest orderRequest)
-	{
-		orderService.placeOrder(orderRequest);
-		return "Order Placed Successfully";
-	}
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+	@TimeLimiter(name = "inventory")
+	@Retry(name = "inventory")
+	 public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+	
+	 public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+	      //  log.info("Cannot Place Order Executing Fallback logic");
+	        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
+	    }
 
 }
